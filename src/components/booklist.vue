7d0753a261be4cddab4aea87e9ec6c81e1c4eb7a
udaysai194 <udaysai194@gmail.com>
2018-02-12 23:44:57 +0530

    deleted several vue default files

---
+++ src/components/booklist.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'booklist',
+  data () {
+    return {
+      msg: 'this is the booklist view'
+    }
+  }
+
+}
+</script>
+
+<style lang="css">
+</style>
